cbe2eb1aa92c2fe3ebd2e96ca819b09b8589a6f6
ashgolan <ashgolan@hotmail.com>
2023-12-25 23:24:58 +0200

    in navbar ...

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,7 +3,13 @@ import "./navbar.css";
 export default function Navbar() {
   return (
     <nav>
+      <button>הגדרות</button>
+      <button>תיקון שטח</button>
+      <button>שרוולים</button>
       <img src="./logo.jpg" alt="" />
+      <button>מכירות</button>
+      <button>הוצאות</button>
+      <button>עסקאות</button>
     </nav>
   );
 }
